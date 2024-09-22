fb72209fb5efc1c59b3e95d5700ce72354ac54a9
Karan Negi <154900804+Karanegi21@users.noreply.github.com>
2024-09-22 21:49:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(express.json());
 app.use('/api', router);
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 // Connect to the database and start the server
 connectDB().then(() => {
